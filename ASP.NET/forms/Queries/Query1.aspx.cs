0969ec1cc137605eaa181015605024239141106e
kovaren <ivankovarin@yandex.ru>
2015-05-25 17:27:25 +0500

    Начал 1 запрос.

--- ASP.NET/forms/Queries/Query1.aspx.cs
+++ ASP.NET/forms/Queries/Query1.aspx.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using ICSSoft.STORMNET.Business;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ICSSoft.STORMNET.Business.LINQProvider;
 
 namespace IIS.EmployeeCourses.forms.Queries
 {
@@ -12,6 +14,33 @@ namespace IIS.EmployeeCourses.forms.Queries
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public void RunQuery()
+        {
+            //var ds = (SQLDataService)DataServiceProvider.DataService; // Сервис данных.
+            //var employees = ds.Query<Журнал>(Журнал.Views.ЖурналL).ToArray();
+            //var logs = ds.Query<Курс>(Курс.Views.КурсL).ToArray();
+            //var query = employees
+            //    .Join(logs,
+            //    j => j.Курс.__PrimaryKey,
+            //    c => c.__PrimaryKey,
+            //    (j, c) => new
+            //    {
+            //        Курс = c.Название,
+            //        Сотрудник = j.Сотрудник,
+            //    })
+            //    .GroupBy(x => new { x.Курс, x.Сотрудник })
+            //    .Select(a => new
+            //    {
+            //        Курс = a.Key.Курс,
+            //        Слушатели = a.Count(y => y.Сотрудник)
+            //    })
+            //    .OrderByDescending(b => b.Средний_балл)
+            //    .ToArray();
+
+            //GridView1.DataSource = query.ToList();
+            //GridView1.DataBind();
         }
     }
 }
--- ASP.NET/forms/Queries/Query1.aspx.designer.cs
+++ ASP.NET/forms/Queries/Query1.aspx.designer.cs
@@ -13,24 +13,6 @@ namespace IIS.EmployeeCourses.forms.Queries {
     public partial class Query1 {
         
         /// <summary>
-        /// firstDate control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::ICSSoft.STORMNET.Web.AjaxControls.DatePicker firstDate;
-        
-        /// <summary>
-        /// lastDate control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::ICSSoft.STORMNET.Web.AjaxControls.DatePicker lastDate;
-        
-        /// <summary>
         /// GridView1 control.
         /// </summary>
         /// <remarks>
