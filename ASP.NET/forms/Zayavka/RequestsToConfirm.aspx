44004bab32613ab33c0a40981df22a87cf9d8c3c
dazzlex27 <>
2015-05-25 17:02:28 +0500

    Ещё поправил формы.

--- ASP.NET/forms/Zayavka/RequestsToConfirm.aspx
+++
@@ -1,8 +0,0 @@
-﻿<%@ Page Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="RequestsToConfirm.aspx.cs" Inherits="IIS.EmployeeCourses.forms.Zayavka.RequestsToConfirm" %>
-<%@ Import namespace="NewPlatform.Flexberry.Web.Page" %>
-
-<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <div>
-    
-    </div>
-</asp:Content>
---
+++ ASP.NET/forms/Zayavka/RequestsToConfirmL.aspx.cs
@@ -0,0 +1,22 @@
+﻿using ICSSoft.STORMNET.Web.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IIS.EmployeeCourses
+{
+    public partial class RequestsToConfirmL : BaseListForm<Заявка>
+    {
+        public RequestsToConfirmL() : base(Заявка.Views.RequestsToConfirmL)
+        {
+            EditPage = RequestsToConfirmL.FormPath;
+        }
+        public static string FormPath
+        {
+            get { return "~/forms/Zayavka/RequestsToConfirmL.aspx"; }
+        }
+    }
+}
